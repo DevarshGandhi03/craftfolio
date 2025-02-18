fb1663ddb0f079073a252cf70070a415ba7522bc
Devarsh <devarsh600@gmail.com>
2025-02-18 22:44:37 +0530

    a

--- src/components/PortfolioThemeComponents/Portfolio_1.jsx
+++ src/components/PortfolioThemeComponents/Portfolio_1.jsx
@@ -18,12 +18,12 @@ function Portfolio_1({ portfolioDetails }) {
   const currentYear = new Date().getFullYear();
 
   useEffect(() => {
-    document.documentElement.style.scrollBehavior = "smooth";
+    window.document.documentElement.style.scrollBehavior = "smooth";
   }, []);
 
   useEffect(() => {
     const handleScroll = () => {
-      const sections = document.querySelectorAll("section");
+      const sections = window.document.querySelectorAll("section");
       let current = "home";
       sections.forEach((section) => {
         const sectionTop = section.offsetTop;
